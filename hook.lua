2a61dd1ae836acd4c0f049830e6428ccf498433f
huttsMichael <huttsmichael@gmail.com>
2022-11-16 14:45:17 -0500

    added frame advance call to client type calls

--- hook.lua
+++ hook.lua
@@ -83,7 +83,12 @@ qtype = {
     INPUT = 0;
     READ = 1;
     WRITE = 2;
-    CLIENT = 3
+    CLIENT = 3;
+}
+
+-- Cient command types
+ctype = {
+	ADVANCE = 0;
 }
 
 -- Response codes
@@ -91,14 +96,15 @@ rcodes = {
 	INPUT 	= 0;  -- Successfully wrote to memory
 	BYTE    = 1;  -- Successfully read byte
 	INTEGER = 2;  -- Successfully read integer
-	FLOAT   = 3;  -- Successfully read float
+	CLIENT  = 3;  -- Successfully controlled client
 	ERROR   = 4;  -- Generic error
 }
 
 
 function format_response(code, message)
-	emu.frameadvance()
+	-- emu.frameadvance()
 	-- Format response code and message into a valid response
+	-- console.log(code, "_", message)
 	return tostring(code) .. '_' .. tostring(message)
 end
 
@@ -116,6 +122,9 @@ local function handleRequest(data)
 	elseif form == qtype["READ"] then
 		query_type, domain, mem_address = string.match(data, "(%d)%/(.+)%/(.+)%/")
 		mem_address = tonumber(mem_address)
+	elseif form == qtype["CLIENT"] then
+		query_type, client_type = string.match(data, "(%d)%/(%d)%/")
+		client_type = tonumber(client_type)
 	end
 
 	query_type = tonumber(query_type)
@@ -140,6 +149,17 @@ local function handleRequest(data)
 			rcodes.BYTE,
 			memory.readbyte(mem_address, domain)
 		)
+	end
+	-- [ CLIENT ]
+	if form == qtype["CLIENT"] then
+		-- [ FRAME ADVANCE ]
+		if client_type == ctype["ADVANCE"] then
+			emu.frameadvance()
+			return format_response(
+				rcodes.CLIENT,
+				true
+			)
+		end
 	end
 
 
